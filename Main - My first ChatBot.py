80c02e3659b2765bd4be20f4e1fd90f770a3ec33
MathieuMdg <151543011+MathieuMdg@users.noreply.github.com>
2023-11-20 16:11:10 +0100

    Main - My first ChatBot.py

--- Main - My first ChatBot.py
+++ Main - My first ChatBot.py
@@ -1,62 +1,50 @@
 import os
+
+
+# Créer une liste avec le nom de tous les fichiers
 def list_of_files(directory, extension):
     files_names = []
     for filename in os.listdir(directory):
         if filename.endswith(extension):
             files_names.append(filename)
     return files_names
+
+
+# Affiche la liste
 def print_list(files_names):
     print(files_names)
 
-def extraction(nom_fichier:list):
-    extraction = []
+
+# Extrait les noms de fichier de la liste et ajoute les noms des présidents dans une nouvelle liste
+def extraction(nom_fichier: list):
+    L = []
     for i in nom_fichier:
         if i[-5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            extraction.append((i[11:-5]))
+            L.append((i[11:-5]))
         else:
-            extraction.append((i[11:-4]))
-    return extraction
+            L.append((i[11:-4]))
+    return L
 
-def sans_doublon(liste:list):
-    sans_doublon = []
+
+# Permet de supprimer les doublons d'une liste
+def sans_doublon(liste: list):
+    L = []
     for i in liste:
-        if i not in sans_doublon:
-            sans_doublon.append(i)
-    return sans_doublon
+        if i not in L:
+            L.append(i)
+    return L
 
+
+# Associe à chaque noms de président un prénom et l'ajoute dans une liste
 def prenom_nom(liste: list):
-    prenom = {"Macron": "Emmanuel", "Mitterrand": "François", "Sarkozy": "Nicolas", "Giscard dEstaing": "Valéry", "Chirac": "Jacques", "Hollande": "François"}
-    prenom_nom = []
+    prenom = {"Macron": "Emmanuel", "Mitterrand": "François", "Sarkozy": "Nicolas", "Giscard dEstaing": "Valéry",
+              "Chirac": "Jacques", "Hollande": "François"}
+    L = []
     for i in liste:
-        prenom_nom.append(prenom[i] + ' ' + i)
-    return prenom_nom
-
-def prenom_nom2(nom:str):
-    prenom = {"Macron": "Emmanuel", "Mitterrand": "François", "Sarkozy": "Nicolas", "Giscard dEstaing": "Valéry", "Chirac": "Jacques", "Hollande": "François"}
-    prenom_nom = ""
-    prenom_nom = prenom[nom]
-    return prenom_nom
+        L.append(prenom[i] + ' ' + i)
+    return L
 
 
 directory = "./speeches"
 files_names = list_of_files(directory, "txt")
 print_list(files_names)
-
-
-print(sans_doublon(extraction(files_names)))
-
-prenom_president = {"Macron" : "Emmanuel", "Mitterrand" : "François", "Sarkozy" : "Nicolas", "Giscard dEstaing" : "Valéry", "Chirac" : "Jacques", "Hollande" : "François"}
-print(prenom_president["Macron"])
-print(sans_doublon(extraction(files_names)))
-
-for i in files_names:
-    fichier1 = open("./speeches/" + i, 'r')
-    fichier2 = open("./speeches/hello.txt", 'w')
-    contenu = fichier1.read()
-    print(contenu)
-    difference = int('a') - 65
-    for elt in contenu:
-        if (elt < 'Z') and (elt > "A"):
-            elt = elt + (difference)
-            print(elt)
-        fichier2.write(elt)
